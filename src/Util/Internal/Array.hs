afdbd4f98c131ea9a1a71d6fb786df2954d20a1a
konsumlamm <konsumlamm@gmail.com>
2021-05-27 18:02:23 +0200

    Rewrite Util.Internal.Array
    Array & MutableArray now store start and length for fast slicing

--- src/Util/Internal/Array.hs
+++ src/Util/Internal/Array.hs
@@ -1,96 +1,161 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UnboxedTuples #-}
 
-module Util.Internal.Array where
+module Util.Internal.Array
+    ( Array, MutableArray
+    , empty, singleton, from2
+    , index, adjust, head, last
+    , take, drop, splitAt
+    , snoc, cons
+    , new, write
+    , freeze
+    ) where
 
+import Control.DeepSeq (NFData(..))
+import Control.Monad (when)
 import Control.Monad.ST
+import Data.Foldable (Foldable(..))
 import Data.Primitive.SmallArray
 import Prelude hiding (take, drop, splitAt, head, last)
 
--- TODO: save start (and end?) index for O(1) slicing
+-- start length array
+data Array a = Array !Int !Int !(SmallArray a)
+data MutableArray s a = MutableArray !Int !Int !(SmallMutableArray s a)
 
-type Array a = SmallArray a
-type MutableArray s a = SmallMutableArray s a
+instance Semigroup (Array a) where
+    Array start1 len1 arr1 <> Array start2 len2 arr2 = Array 0 len' $ runSmallArray $ do
+        sma <- newSmallArray len' uninitialized
+        copySmallArray sma 0 arr1 start1 len1
+        copySmallArray sma len1 arr2 start2 len2
+        pure sma
+      where
+        !len' = len1 + len2
+
+instance Functor Array where
+    fmap f (Array start len arr) = Array 0 len $ runSmallArray $ do
+        sma <- newSmallArray len uninitialized
+        let loop i j = when (j < len) $ do
+                x <- indexSmallArrayM arr i
+                writeSmallArray sma j (f x)
+                loop (i + 1) (j + 1)
+        loop start 0
+        pure sma
+
+instance Foldable Array where
+    foldr f = \z (Array start len arr) ->
+        let !end = start + len
+            go i
+                | i == end = z
+                | (# x #) <- indexSmallArray## arr i = f x (go (i + 1))
+        in go start
+    {-# INLINE foldr #-}
+
+    foldl f = \z (Array start len arr) ->
+        let go i
+                | i < start = z
+                | (# x #) <- indexSmallArray## arr i = f (go (i - 1)) x
+        in go (start + len - 1)
+    {-# INLINE foldl #-}
+
+    foldr' f = \z (Array start len arr) ->
+        let go i !acc
+                | i < start = acc
+                | (# x #) <- indexSmallArray## arr i = go (i - 1) (f x acc)
+        in go (start + len - 1) z
+    {-# INLINE foldr' #-}
+
+    foldl' f = \z (Array start len arr) ->
+        let !end = start + len
+            go i !acc
+                | i == end = acc
+                | (# x #) <- indexSmallArray## arr i = go (i + 1) (f acc x)
+        in go start z
+    {-# INLINE foldl' #-}
+
+    null arr = length arr == 0
+    {-# INLINE null #-}
+
+    length (Array _ len _) = len
+    {-# INLINE length #-}
+
+instance Traversable Array where
+    -- TODO: optimize?
+    traverse f arr = Array 0 len . smallArrayFromListN len <$> traverse f (toList arr)
+      where
+        !len = length arr
+
+instance (NFData a) => NFData (Array a) where
+    rnf = foldl' (\_ x -> rnf x) ()
 
 uninitialized :: a
 uninitialized = errorWithoutStackTrace "uninitialized"
 
 empty :: Array a
-empty = create $ newSmallArray 0 uninitialized
+empty = Array 0 0 $ runSmallArray (newSmallArray 0 uninitialized)
 
 singleton :: a -> Array a
-singleton x = create $ newSmallArray 1 x
-{-# INLINE singleton #-}
+singleton x = Array 0 1 $ runSmallArray (newSmallArray 1 x)
 
 from2 :: a -> a -> Array a
-from2 x y = create $ do
-    arr <- newSmallArray 2 x
-    writeSmallArray arr 1 y
-    pure arr
-{-# INLINE from2 #-}
-
-fromList :: [a] -> Array a
-fromList = smallArrayFromList
-{-# INLINE fromList #-}
+from2 x y = Array 0 2 $ runSmallArray $ do
+    sma <- newSmallArray 2 x
+    writeSmallArray sma 1 y
+    pure sma
 
 index :: Array a -> Int -> a
-index = indexSmallArray
-{-# INLINE index #-}
+index (Array start _ arr) idx = indexSmallArray arr (start + idx)
 
 adjust :: Array a -> Int -> (a -> a) -> Array a
-adjust arr idx f = create $ do
-    newArr <- thawSmallArray arr 0 (length arr)
-    let x = indexSmallArray arr idx
-    writeSmallArray newArr idx (f x)
-    pure newArr
-{-# INLINE adjust #-}
+adjust (Array start len sa) idx f = Array 0 len $ runSmallArray $ do
+    sma <- thawSmallArray sa start len
+    x <- indexSmallArrayM sa (start + idx)
+    writeSmallArray sma idx (f x)
+    pure sma
 
 take :: Array a -> Int -> Array a
-take arr = cloneSmallArray arr 0
-{-# INLINE take #-}
+take (Array start _ arr) n = Array start n arr
 
 drop :: Array a -> Int -> Array a
-drop arr n = cloneSmallArray arr n (length arr - n)
-{-# INLINE drop #-}
+drop (Array start len arr) n = Array (start + n) (len - n) arr
 
 splitAt :: Array a -> Int -> (Array a, Array a)
 splitAt arr idx = (take arr idx, drop arr idx)
-{-# INLINE splitAt #-}
 
 head :: Array a -> a
 head arr = index arr 0
-{-# INLINE head #-}
 
 last :: Array a -> a
 last arr = index arr (length arr - 1)
-{-# INLINE last #-}
 
 snoc :: Array a -> a -> Array a
-snoc arr x = create $ do
-    newArr <- newSmallArray (length arr + 1) x
-    copySmallArray newArr 0 arr 0 (length arr)
-    pure newArr
-{-# INLINE snoc #-}
+snoc (Array _ len arr) x = Array 0 len' $ runSmallArray $ do
+    sma <- newSmallArray len' x
+    copySmallArray sma 0 arr 0 len
+    pure sma
+  where
+    !len' = len + 1
 
-create :: (forall s. ST s (MutableArray s a)) -> Array a
-create = runSmallArray
-{-# INLINE create #-}
+cons :: Array a -> a -> Array a
+cons (Array _ len arr) x = Array 0 len' $ runSmallArray $ do
+    sma <- newSmallArray len' x
+    copySmallArray sma 1 arr 0 len
+    pure sma
+  where
+    !len' = len + 1
 
 new :: Int -> ST s (MutableArray s a)
-new len = newSmallArray len uninitialized
-{-# INLINE new #-}
+new len = MutableArray 0 len <$> newSmallArray len uninitialized
 
 read :: MutableArray s a -> Int -> ST s a
-read = readSmallArray
-{-# INLINE read #-}
+read (MutableArray start _ arr) idx = readSmallArray arr (start + idx)
 
 write :: MutableArray s a -> Int -> a -> ST s ()
-write = writeSmallArray
-{-# INLINE write #-}
+write (MutableArray start _ arr) idx = writeSmallArray arr (start + idx)
 
 freeze :: MutableArray s a -> Int -> Int -> ST s (Array a)
-freeze = freezeSmallArray
-{-# INLINE freeze #-}
+freeze (MutableArray start _ arr) idx len = Array 0 len <$> freezeSmallArray arr (start + idx) len
 
 thaw :: Array a -> Int -> Int -> ST s (MutableArray s a)
-thaw = thawSmallArray
-{-# INLINE thaw #-}
+thaw (Array start _ arr) idx len = MutableArray 0 len <$> thawSmallArray arr (start + idx) len
